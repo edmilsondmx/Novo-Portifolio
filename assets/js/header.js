06735c90dfe07ab67790e7359b9a7667ba0d4a53
Edmilson Gomes <edmilsondmx@gmail.com>
2023-02-26 14:14:37 -0300

    altera subtitulo para dispositivos menores

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,29 +1,29 @@
 const content = "Full-Stack Developer";
-        const text = document.getElementById("p");
-        const speed = 200;
-        const cursor = document.querySelector('.cursor');
-        cursor.classList.toggle('ativo');
-        let cont = 0;
-        function maqEscrever(){
-            
-            if(cont < content.length){
-                text.textContent += content.charAt(cont);
-                cont++;
-                setTimeout(maqEscrever, speed);
-            }
-            
-        }
-        maqEscrever()
+const text = document.getElementById("p");
+const speed = 200;
+const cursor = document.querySelector('.cursor');
+cursor.classList.toggle('ativo');
+let cont = 0;
+function maqEscrever(){
+    
+    if(cont < content.length){
+        text.textContent += content.charAt(cont);
+        cont++;
+        setTimeout(maqEscrever, speed);
+    }
+    
+}
+maqEscrever()
         
-        function initCursorBlink() {
-            
+function initCursorBlink() {
+    
 
-            function animaCursor() {
-                cursor.classList.toggle('ativo');
-            }
-            setInterval(animaCursor, 500);
-        };
+    function animaCursor() {
+        cursor.classList.toggle('ativo');
+    }
+    setInterval(animaCursor, 500);
+};
 
-        setTimeout(() => {
-            initCursorBlink() 
-        }, 2500);
+setTimeout(() => {
+    initCursorBlink() 
+}, 2500);
